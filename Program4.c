ef11e0a6c65b86f2586f280d9af19173c9a79a38
Harvi Patel <73467549+harvvii@users.noreply.github.com>
2021-06-14 10:19:13 +0530

    Create Program4.c

---
+++ Program4.c
@@ -0,0 +1,35 @@
+/*
+This file contains code for "Reading a file and printing its content on console/terminal."
+*/
+
+#include <stdio.h>
+#include <stdlib.h> // For exit()
+
+int main()
+{
+    FILE *fptr;
+
+    char filename[100], c;
+
+    printf("Enter the filename to open \n");
+    scanf("%s", filename);
+
+    // Open file
+    fptr = fopen(filename, "r");
+    if (fptr == NULL)
+    {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+
+    // Read contents from file
+    c = fgetc(fptr);
+    while (c != EOF)
+    {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+
+    fclose(fptr);
+    return 0;
+}
